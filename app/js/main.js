c9b7d44f153b4acc1192e27c04d21bf2a8736a24
KanishVlad <130839651+KanishVlad@users.noreply.github.com>
2023-07-08 16:10:19 +0300

    added features restaurant testimonial app sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,12 @@
 $(function(){
 
+  $('.testimonials__slider').slick({
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"><svg class="slick-icon" width="37" height="37"><use href="images/sprite.svg#icon-prev-arrow"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="slick-icon" width="37" height="37"><use href="images/sprite.svg#icon-next-arrow"></use></svg></button>',
+    infinite: false,
+  })
+
   $(window).on('scroll', function () {
     if ($(window).scrollTop() > 0) {
       $('.header').addClass('header--scroll');
